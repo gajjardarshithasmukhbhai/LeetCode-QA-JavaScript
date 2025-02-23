da32fbb275860069d2a699af38b15cb08c3d154a
gajjardarshithasmukhbhai <darshit.hasmukhbhai1998@gmail.com>
2025-02-24 00:02:06 +0530

    minor change

--- Dynamic-Programming 🧠/M-House-Robber-Problem(Pick-nonPick)/dp.js
+++ Dynamic-Programming 🧠/M-House-Robber-Problem(Pick-nonPick)/dp.js
@@ -1,5 +1,6 @@
 const homeList = [2,3,2,4,6];
 
+// In Recursion, overlapping happens. so we need to do memoization for that
 const maximumNonAdjacentSum = (values) => {
 
    const memoizaion = Array(values.length).fill(-1);
@@ -11,6 +12,9 @@ const maximumNonAdjacentSum = (values) => {
         return 0;
     }
 
+    if(memoizaion[index]!==-1) {
+        return memoizaion[index];
+    }
     let pick = values[index] + helper(values, index+2);
     let nonPick = helper(values, index+1);
 
@@ -21,6 +25,7 @@ const maximumNonAdjacentSum = (values) => {
    // this removed the last element 
    let result1 = helper(values.slice(0, values.length-1),0)
    
+   memoizaion = Array(values.length).fill(-1);
    // this removed the first element
    let result2 = helper(values.slice(1),0)
 
