f149f859b9f7193fb78b81540856f6b367927b54
gajjardarshithasmukhbhai <darshit.hasmukhbhai1998@gmail.com>
2025-04-18 18:28:00 +0530

    left side view

---
+++ Binary-Tree 🧠/M-199-Left-View-Binary-Tree ⭐/index.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+const leftSideView = (root) => {
+    const stack = [];
+    if(root === null)  return [];
+     
+    const helper = (node, level) => {
+
+        if(node!==null) return;
+
+        if(stack[level] === undefined ) {
+            stack.push(node.val);
+        }
+        helper(node.left, level+1)
+        helper(node.right, level+1);
+    }
+    helper(root, 0);
+};
