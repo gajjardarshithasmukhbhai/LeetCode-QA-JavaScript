f0fcfcf45420b95c213da6e6b59c3f4822f2507b
gajjardarshithasmukhbhai <darshit.hasmukhbhai1998@gmail.com>
2025-05-20 08:43:21 +0530

    new added

---
+++ Two-Pointer-Approach 🧠/M-Longest Substring With At Most K Distinct Characters/index.js
@@ -0,0 +1,24 @@
+const longestKDistictEle = (nums) => {
+
+    let left = 0;
+    let right = 0;
+    let maxWindowLen = 0;
+    let hasMap = new Map();
+
+    while(right < nums.length) {
+        hasMap.set(nums[right], (hasMap.get(nums[right]) || 0) + 1);
+
+        while(hasMap.size > 2) {
+            hasMap.set(nums[right], hasMap.get(nums[right])-1);
+            if(hasMap.get(nums[right])===0) {
+                hasMap.delete(nums[right]);
+            }
+            left++;
+        }
+        maxWindowLen = Math.max(maxWindowLen, right-left+1);
+        right++;
+    }
+    return maxWindowLen;
+}
+
+console.log(longestKDistictEle([1,2,1,2,3])); // Output: 4 (subarray: [1,2,1,2])
